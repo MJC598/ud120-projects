3b183345ba8857f3dcd58208e4fd06d2c7fa9a3a
Matthew Carroll <matthew.j.carroll@outlook.com>
2019-01-17 14:58:16 -0600

    added naive_bayes fit, prediction, and accuracy test to nb_author_id

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,19 +14,24 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
-
-
-
-
 #########################################################
 ### your code goes here ###
+### added some imports above for code readability ###
+if __name__ == '__main__':
+    main()
 
+def main():
+    features_train, features_test, labels_train, labels_test = preprocess()
+    classifier = GaussianNB()
+    classifier.fit(features_train, labels_train)
+    pred = classifier.predict(features_test)
+    print(accuracy_score(pred, labels_test))
 
 #########################################################
 
